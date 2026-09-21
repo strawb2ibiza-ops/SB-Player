8bcc6145124e62b43c1187679b79ce3d3cd2360b
strawb2ibiza-ops <strawb2ibiza@gmail.com>
2026-09-21 03:48:05 +0100

    Add SB Player v0.3 source

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:media_kit/media_kit.dart';
+import 'package:window_manager/window_manager.dart';
+
+import 'src/app.dart';
+import 'src/config/app_config.dart';
+import 'src/services/secure_account_store.dart';
+import 'src/state/app_controller.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  MediaKit.ensureInitialized();
+
+  if (Platform.isWindows) {
+    await windowManager.ensureInitialized();
+    const options = WindowOptions(
+      size: Size(1280, 800),
+      minimumSize: Size(900, 600),
+      center: true,
+      title: 'SB Player',
+    );
+    await windowManager.waitUntilReadyToShow(options, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
+
+  final config = AppConfig.fromEnvironment();
+  final controller = AppController(
+    config: config,
+    accountStore: const SecureAccountStore(),
+  );
+
+  await controller.restoreSession();
+  runApp(SbPlayerApp(controller: controller));
+}
