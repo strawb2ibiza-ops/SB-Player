2d0fc6ca408a7f2b02a0507950becc7005634cdc
strawb2ibiza-ops <strawb2ibiza@gmail.com>
2026-09-21 03:49:09 +0100

    Add SB Player v0.3 models and services

---
+++ lib/src/models/vod_item.dart
@@ -0,0 +1,25 @@
+class VodItem {
+  const VodItem({
+    required this.id,
+    required this.name,
+    required this.streamUrl,
+    required this.categoryId,
+    this.posterUrl,
+    this.extension,
+    this.plot,
+    this.rating,
+    this.releaseDate,
+    this.duration,
+  });
+
+  final String id;
+  final String name;
+  final String streamUrl;
+  final String categoryId;
+  final String? posterUrl;
+  final String? extension;
+  final String? plot;
+  final double? rating;
+  final String? releaseDate;
+  final String? duration;
+}
